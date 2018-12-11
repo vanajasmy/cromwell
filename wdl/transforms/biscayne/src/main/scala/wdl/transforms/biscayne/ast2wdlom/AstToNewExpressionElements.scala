5d464f0968dc66a5eba19b47ac0eed5d488136c1
Chris Llanwarne <cjllanwarne@gmail.com>
2018-12-11 16:51:05 -0500

    Also disallow read_objects and write_objects

--- wdl/transforms/biscayne/src/main/scala/wdl/transforms/biscayne/ast2wdlom/AstToNewExpressionElements.scala
+++ wdl/transforms/biscayne/src/main/scala/wdl/transforms/biscayne/ast2wdlom/AstToNewExpressionElements.scala
@@ -13,6 +13,8 @@ object AstToNewExpressionElements {
     "as_pairs" -> AstNodeToExpressionElement.validateOneParamEngineFunction(AsPairs, "as_pairs"),
     "collect_by_key" -> AstNodeToExpressionElement.validateOneParamEngineFunction(CollectByKey, "collect_by_key"),
     "read_object" -> (_ => "read_object is no longer available in this WDL version. Consider using read_json instead".invalidNel),
+    "read_objects" -> (_ => "read_objects is no longer available in this WDL version. Consider using read_json instead".invalidNel),
     "write_object" -> (_ => "write_object is no longer available in this WDL version. Consider using write_json instead".invalidNel),
+    "write_object" -> (_ => "write_objects is no longer available in this WDL version. Consider using write_json instead".invalidNel),
   )
 }
